b8b1291270040b4ce3224690c31977c72f626a7b
Yusra-AlRahbi <yusra5273@gmail.com>
2024-09-19 09:50:20 +0400

    Modify Card List to  make it dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import Category from "./components/Category";
 import CardList from "./components/CardList";
+import { item } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [itemData,setItemData]= useState(item)
+
+
   return (
     <div className="color_body">
       <NavBar />
       <Container>
         <Header />
         <Category/>
-        <CardList/>
-        <CardList/>
-        <CardList/>
+        <CardList itemData={itemData}/>
       </Container>
     </div>
   );
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,34 +1,38 @@
 import { Card, Col, Row } from "react-bootstrap";
 
-function CardList() {
+const CardList = ({ itemData }) => {
   return (
     <Row>
-      <Col sm="12" className="mb-3">
-        <Card className="d-flex flex-row">
-          <Card.Img
-            variant="top"
-            src="../assets/img/pizza.jpg"
-            className="img-item"
-          />
-          <Card.Body>
-            <Card.Title className=" d-flex justify-content-between">
-              <div className="item-title">PIZZA</div>
-              <div className="item-price">1 OR</div>
-
-              </Card.Title>
-            <Card.Text className="py-2">
-              <div className="item-description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Doloribus adipisci incidunt ut distinctio nihil exercitationem
-                illo officia, debitis veniam impedit iusto praesentium minus
-                reiciendis sint doloremque et aspernatur blanditiis modi!
-              </div>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
+      {itemData.length >= 1 ? (
+        itemData.map((item) => {
+          return (
+            <Col key={item.id} sm="12" className="mb-3">
+              <Card className="d-flex flex-row">
+                <Card.Img
+                  variant="top"
+                  src={item.Img}
+                  className="img-item"
+                />
+                <Card.Body>
+                <Card.Title className=" d-flex justify-content-between">
+                    <div className="item-title">{item.title}</div>
+                    <div className="item-price">{item.price}</div>
+                  </Card.Title>
+                  <Card.Text className="py-2">
+                    <div className="item-description">
+                    {item.description}
+                    </div>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })
+      ) : (
+        <h3 className=" text-center"> No data</h3>
+      )}
     </Row>
   );
-}
+};
 
 export default CardList;
